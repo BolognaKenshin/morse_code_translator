087c734e6b876213492c19807b1191320c13d898
Carl <cmillardv77@gmail.com>
2024-12-12 17:18:51 -0500

    Basic translation program, gitignore added

---
+++ main.py
@@ -0,0 +1,18 @@
+int_morse = {"a": ".-", "b": "-...", "c": "-.-.", "d": "-..", "e": ".", "f": "..-.", "g": "--.", "h": "....", "i": "..",
+                                  "j": ".---", "k": "-.-", "l": ".-..", "m": "--", "n": "-.", "o": "---", "p": ".--.", "q": "--.-", "r": ".-.",
+                                  "s": "...", "t": "-", "u": "..-", "v": "...-", "w": ".--", "x": "-..-", "y": "-.--", "z": "--..", "1": ".----",
+                                  "2": "..---", "3": "...--", "4": "....-", "5": ".....", "6": "-....", "7": "--...", "8": "---..", "9": "----.", "0": "-----"}
+
+
+text = input("Please input your text for international morse code translation (letters and numbers only): ")
+text_to_translate = text.lower().split()
+
+translated_code = ""
+
+for word in text_to_translate:
+    for char in word:
+        morse_char = int_morse[char] + " "
+        translated_code += morse_char
+    translated_code += "/ "
+
+print(translated_code)
