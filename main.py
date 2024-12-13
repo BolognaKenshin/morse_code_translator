921dd6cbe5dc824746138b6b9e511a933927a77b
Carl <cmillardv77@gmail.com>
2024-12-13 16:11:50 -0500

    Reverse translation development - test

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ reverse_int_morse = {
     "..---": "2", "...--": "3", "....-": "4", ".....": "5", "-....": "6", "--...": "7", "---..": "8", "----.": "9", "-----": "0"
 }
 
+
+# Functions
 def text_to_morse():
     text = input("Please input your text for international morse code translation (letters and numbers only): ")
     text_to_translate = text.lower().split()
@@ -24,12 +26,36 @@ def text_to_morse():
 
 def morse_to_text():
     morse = input("Please input your morse code for translation: ")
+    morse = morse.split(" ")
     translated_text = ""
     for char in morse:
         if char == "/":
             translated_text += " "
-        else:
+        if char in reverse_int_morse:
             translated_text += reverse_int_morse[char]
+        else:
+            continue
+
     print(f"Morse entered: {morse}")
     print(f"Translated to text: {translated_text}")
 
+
+# Program begins below
+translate = True
+while translate:
+    morse_or_text = input("Would you like to translate text, or translate Morse? (Morse/Text) ")
+    morse_or_text = morse_or_text.lower()
+    if morse_or_text == "text":
+        print("Translating Text...")
+        text_to_morse()
+        answer = input("Would you like to translate again? ").lower()
+        if answer == "no":
+            translate = False
+    elif morse_or_text == "morse":
+        print("Translating morse...")
+        morse_to_text()
+        answer = input("Would you like to translate again? ").lower()
+        if answer == "no":
+            translate = False
+    else:
+        print("That's not a valid input. Please try again.")
