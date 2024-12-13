7c011cdbb5257394832c4711446567bd471d949f
Carl <cmillardv77@gmail.com>
2024-12-13 15:43:32 -0500

    Reverse translation development - test

--- main.py
+++ main.py
@@ -10,16 +10,26 @@ reverse_int_morse = {
     "..---": "2", "...--": "3", "....-": "4", ".....": "5", "-....": "6", "--...": "7", "---..": "8", "----.": "9", "-----": "0"
 }
 
+def text_to_morse():
+    text = input("Please input your text for international morse code translation (letters and numbers only): ")
+    text_to_translate = text.lower().split()
+    translated_code = ""
+    for word in text_to_translate:
+        for char in word:
+            morse_char = int_morse[char] + " "
+            translated_code += morse_char
+        translated_code += "/ "
+    print(f"Text entered: {text}")
+    print(f'Morse Code: {translated_code}')
 
-text = input("Please input your text for international morse code translation (letters and numbers only): ")
-text_to_translate = text.lower().split()
+def morse_to_text():
+    morse = input("Please input your morse code for translation: ")
+    translated_text = ""
+    for char in morse:
+        if char == "/":
+            translated_text += " "
+        else:
+            translated_text += reverse_int_morse[char]
+    print(f"Morse entered: {morse}")
+    print(f"Translated to text: {translated_text}")
 
-translated_code = ""
-
-for word in text_to_translate:
-    for char in word:
-        morse_char = int_morse[char] + " "
-        translated_code += morse_char
-    translated_code += "/ "
-
-print(translated_code)
